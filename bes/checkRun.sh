fb72eb0c37b0d1627218e23429742d2ad3a41322
Zhaoyang Yuan <yuanchy8@mail2.sysu.edu.cn>
2022-03-13 01:15:21 +0800

    bes ok

--- bes/checkRun.sh
+++ bes/checkRun.sh
@@ -1,31 +1,36 @@
-isRunSuccess()
-{
+# isRunSuccess()
+# {
 
-    echo "path is:" $(pwd)
-    dir="./joblist"
-    numAna=`ls $dir | grep -c txt$`
-    echo number of \*.txt is: $numAna
+#     echo "path is:" $(pwd)
+#     dir="./joblist"
+#     numAna=`ls $dir | grep -c txt$`
+#     echo number of \*.txt is: $numAna
 
-    numLog=`ls $dir | grep -c bosslog`
-    echo number of bosslog is: $numLog
+#     numLog=`ls $dir | grep -c bosslog`
+#     echo number of bosslog is: $numLog
 
-    numStopSuccess=`tail $dir/*bosslog | grep -c "Application Manager Stopped successfully" `
-    echo number of stopped successfully is: $numStopSuccess
+#     numStopSuccess=`tail $dir/*bosslog | grep -c "Application Manager Stopped successfully" `
+#     echo number of stopped successfully is: $numStopSuccess
 
-    numFinalizeSuccess=`tail $dir/*bosslog | grep -c "Application Manager Finalized successfully" `
-    echo number of finalized successfully is: $numFinalizeSuccess
+#     numFinalizeSuccess=`tail $dir/*bosslog | grep -c "Application Manager Finalized successfully" `
+#     echo number of finalized successfully is: $numFinalizeSuccess
 
-    numTerminateSuccess=`tail $dir/*bosslog | grep -c "Application Manager Terminated successfully" `
-    echo number of terminated successfully is: $numTerminateSuccess
+#     numTerminateSuccess=`tail $dir/*bosslog | grep -c "Application Manager Terminated successfully" `
+#     echo number of terminated successfully is: $numTerminateSuccess
 
-    echo
-    echo done ~!
-}
+#     echo
+#     echo done ~!
+# }
 
 findRunWrong()
 {
-    echo "path is:" $(pwd)
-    dir="./joblist"
+    
+    dir=${1}
+    if [ "x$dir" == "x" ] 
+    then
+        dir="./joblist"
+    fi
+    echo "path is:" $(pwd)/${dir}
 
     numAna=`ls $dir | grep -c txt$`
     printf "number of *.txt is:\033[31m $numAna \033[0m \n"
@@ -60,170 +65,81 @@ findRunWrong()
 
 }
 
-mycheckrun3()
-{
-	if [ x"${1}" == x"" ]
-	then 
-		echo "error ~! please run like: checkrun \"******\" "
-		return 1
-	fi
-    mtxt=${1}
-    
-    cd 4600/ana/
-    echo "4600______________________________________________________________"
-    a=`tail joblist/*log | grep -c "${mtxt}"`
-    printf "num of ${mtxt}:\033[31m ${a} \033[0m \n"
-    findRunWrong
-    cd ../../
-    
-    cd 4612/ana/
-    echo "4612______________________________________________________________"
-    a=`tail joblist/*log | grep -c "${mtxt}"`
-    printf "num of ${mtxt}:\033[31m ${a} \033[0m \n"
-    findRunWrong
-    cd ../../
-    
-    cd 462*/ana/
-    echo "462*______________________________________________________________"
-    a=`tail joblist/*log | grep -c "${mtxt}"`
-    printf "num of ${mtxt}:\033[31m ${a} \033[0m \n"
-    findRunWrong
-    cd ../../
-    
-    cd 4640/ana/
-    echo "4640____________________________________________________________"
-    a=`tail joblist/*log | grep -c "${mtxt}"`
-    printf "num of ${mtxt}:\033[31m ${a} \033[0m \n"
-    findRunWrong
-    cd ../../
-    
-    cd 4660/ana/
-    echo "4660____________________________________________________________"
-    a=`tail joblist/*log | grep -c "${mtxt}"`
-    printf "num of ${mtxt}:\033[31m ${a} \033[0m \n"
-    findRunWrong
-    cd ../../
-    
-    cd 4680/ana/ 
-    echo "4680____________________________________________________________"
-    a=`tail joblist/*log | grep -c "${mtxt}"`
-    printf "num of ${mtxt}:\033[31m ${a} \033[0m \n"
-    findRunWrong
-    cd ../../
-    
-    cd 4700/ana/ 
-    echo "4700____________________________________________________________"
-    a=`tail joblist/*log | grep -c "${mtxt}"`
-    printf "num of ${mtxt}:\033[31m ${a} \033[0m \n"
-    findRunWrong
-    cd ../../
-} 
-
+energy=(4600 4612 4620 4640 4660 4680 4700)
+#energy=(4740 4750 4780 4840 4914 4946)  # ${energy[0]}
 
 mycheckrun2()
-{
-	if [ x"${1}" == x"" ]
+{   
+    dir=${1}
+    if [ "x$dir" == "x" ] 
+    then
+        dir="./joblist"
+    fi
+
+	if [ x"${2}" == x"" ]
 	then 
 		echo "error ~! please run like: checkrun \"******\" "
 		return 1
 	fi
-    mtxt=${1}
+    mtxt=${2}
     
-    cd 4600/
-    echo "4600______________________________________________________________"
-    a=`tail joblist/*log | grep -c "${mtxt}"`
-    printf "num of ${mtxt}:\033[31m ${a} \033[0m \n"
-    findRunWrong
-    cd ../
-    
-    cd 4612/
-    echo "4612______________________________________________________________"
-    a=`tail joblist/*log | grep -c "${mtxt}"`
-    printf "num of ${mtxt}:\033[31m ${a} \033[0m \n"
-    findRunWrong
-    cd ../
-    
-    cd 462*/
-    cd 462*/
-    echo "462*______________________________________________________________"
-    a=`tail joblist/*log | grep -c "${mtxt}"`
-    printf "num of ${mtxt}:\033[31m ${a} \033[0m \n"
-    findRunWrong
-    cd ../
-    
-    cd 4640/
-    echo "4640____________________________________________________________"
-    a=`tail joblist/*log | grep -c "${mtxt}"`
-    printf "num of ${mtxt}:\033[31m ${a} \033[0m \n"
-    findRunWrong
-    cd ../
-    
-    cd 4660/
-    echo "4660____________________________________________________________"
-    a=`tail joblist/*log | grep -c "${mtxt}"`
-    printf "num of ${mtxt}:\033[31m ${a} \033[0m \n"
-    findRunWrong
-    cd ../
-    
-    cd 4680 
-    echo "4680____________________________________________________________"
-    a=`tail joblist/*log | grep -c "${mtxt}"`
-    printf "num of ${mtxt}:\033[31m ${a} \033[0m \n"
-    findRunWrong
-    cd ../
-    
-    cd 4700 
-    echo "4700____________________________________________________________"
-    a=`tail joblist/*log | grep -c "${mtxt}"`
-    printf "num of ${mtxt}:\033[31m ${a} \033[0m \n"
-    findRunWrong
-    cd ../
+    for i_e in ${energy[@]}
+    do 
+        if [ -d "$i_e/ana" ]; then
+            cd $i_e/ana/
+        else 
+            cd $i_e/
+        fi
+        echo "______________  $i_e ______________"
+        a=`tail $dir/*log | grep -c "${mtxt}"`
+        printf "num of ${mtxt}:\033[31m ${a} \033[0m \n"
+        findRunWrong $dir
+
+        if [ -d "../ana" ]; then
+            cd ../../
+        else 
+            cd ../
+        fi
+    done
 } 
 
 
 mycheckrun()
 {
-	if [ x"${1}" == x"" ]
+    proc=(LL data hadron signal shape )  # ${proc[0]}
+
+    dir=${1}
+    if [ "x$dir" == "x" ] 
+    then
+        dir="./joblist"
+    fi
+
+	if [ x"${2}" == x"" ]
 	then 
 		echo "error ~! please run like: checkrun \"******\" "
 		return 1
 	fi
-    mtxt=${1}
-    
-    cd LL/
-    echo "LL______________________________________________________________"
-    a=`tail joblist/*log | grep -c "${mtxt}"`
-    printf "num of ${mtxt}:\033[31m ${a} \033[0m \n"
-    findRunWrong
-    cd ../
-    
-    cd data/
-    echo "data____________________________________________________________"
-    a=`tail joblist/*log | grep -c "${mtxt}"`
-    printf "num of ${mtxt}:\033[31m ${a} \033[0m \n"
-    findRunWrong
-    cd ../
-    
-    cd hadron/
-    echo "hadron____________________________________________________________"
-    a=`tail joblist/*log | grep -c "${mtxt}"`
-    printf "num of ${mtxt}:\033[31m ${a} \033[0m \n"
-    findRunWrong
-    cd ../
-    
-    cd signal/ana/
-    echo "signal____________________________________________________________"
-    a=`tail joblist/*log | grep -c "${mtxt}"`
-    printf "num of ${mtxt}:\033[31m ${a} \033[0m \n"
-    findRunWrong
-    cd ../../
-    
-    cd shape/ana/
-    echo "shape____________________________________________________________"
-    a=`tail joblist/*log | grep -c "${mtxt}"`
-    printf "num of ${mtxt}:\033[31m ${a} \033[0m \n"
-    findRunWrong
-    cd ../../
+    mtxt=${2}
+        
+
+    for i_e in ${proc[@]}
+    do 
+        if [ ! -d "$i_e/ana/" ]; then
+            cd $i_e/ana/
+        else 
+            cd $i_e/
+        fi
+        echo "______________  $i_e ______________"
+        a=`tail $dir/*log | grep -c "${mtxt}"`
+        printf "num of ${mtxt}:\033[31m ${a} \033[0m \n"
+        findRunWrong $dir
+
+        if [ -d "$i_e/ana/" ]; then
+            cd ../../
+        else 
+            cd ../
+        fi
+    done
 } 
 
 
